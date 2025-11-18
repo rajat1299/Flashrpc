2a240f30ef46e5f4cf545b56a2559e5fc7bf2f01
rajat1299 <146774710+rajat1299@users.noreply.github.com>
2025-11-18 13:04:53 -0600

    Step 9.2: Add fasterpc imports

--- fasterpc/tests/fastapi_dependency_test.py
+++ fasterpc/tests/fastapi_dependency_test.py
@@ -14,3 +14,7 @@ from fastapi import FastAPI, WebSocket, Header, HTTPException, Depends
 
 from websockets.exceptions import InvalidStatus
 
+from fasterpc.rpc_methods import RpcUtilityMethods
+
+from fasterpc.websocket_rpc_client import WebSocketRpcClient
+
