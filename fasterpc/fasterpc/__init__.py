272611da4188144b4ffaae5eb2d939e90ad4cde4
rajat1299 <146774710+rajat1299@users.noreply.github.com>
2025-11-18 12:38:45 -0600

    Step 4.3: Add __init__.py with public API exports

---
+++ fasterpc/fasterpc/__init__.py
@@ -0,0 +1,10 @@
+from .rpc_methods import RpcMethodsBase, RpcUtilityMethods
+
+from .websocket_rpc_client import WebSocketRpcClient
+
+from .websocket_rpc_endpoint import WebsocketRPCEndpoint
+
+from .rpc_channel import RpcChannel
+
+from .logger import logging_config, LoggingModes, get_logger
+
