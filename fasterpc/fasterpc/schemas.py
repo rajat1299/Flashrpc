93b17202db7ea554f25760cc3fc0b83bec6768f1
rajat1299 <146774710+rajat1299@users.noreply.github.com>
2025-11-18 12:31:09 -0600

    Step 2.3: Add schemas.py with RPC message data models

---
+++ fasterpc/fasterpc/schemas.py
@@ -0,0 +1,66 @@
+from enum import Enum
+
+from typing import Dict, Generic, Optional, TypeVar
+
+from pydantic import BaseModel
+
+from .utils import is_pydantic_pre_v2
+
+
+
+UUID = str
+
+
+
+class RpcRequest(BaseModel):
+
+    method: str
+
+    arguments: Optional[Dict] = {}
+
+    call_id: Optional[UUID] = None
+
+
+
+ResponseT = TypeVar("ResponseT")
+
+
+
+if is_pydantic_pre_v2():
+
+    from pydantic.generics import GenericModel
+
+    class RpcResponse(GenericModel, Generic[ResponseT]):
+
+        result: ResponseT
+
+        result_type: Optional[str]
+
+        call_id: Optional[UUID] = None
+
+else:
+
+    class RpcResponse(BaseModel, Generic[ResponseT]):
+
+        result: ResponseT
+
+        result_type: Optional[str]
+
+        call_id: Optional[UUID] = None
+
+
+
+class RpcMessage(BaseModel):
+
+    request: Optional[RpcRequest] = None
+
+    response: Optional[RpcResponse] = None
+
+
+
+class WebSocketFrameType(str, Enum):
+
+    Text = "text"
+
+    Binary = "binary"
+
