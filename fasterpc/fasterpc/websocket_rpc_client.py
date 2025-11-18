5919d7baa05d0704f2565f4265b776559bf53e08
rajat1299 <146774710+rajat1299@users.noreply.github.com>
2025-11-18 12:43:03 -0600

    Rewrite websocket_rpc_client.py - import logger and simplewebsocket

--- fasterpc/fasterpc/websocket_rpc_client.py
+++ fasterpc/fasterpc/websocket_rpc_client.py
@@ -13,3 +13,7 @@ from tenacity import retry, RetryCallState, wait, retry_if_exception
 from .rpc_methods import PING_RESPONSE, RpcMethodsBase
 
 from .rpc_channel import RpcChannel, OnConnectCallback, OnDisconnectCallback
+
+from .logger import get_logger
+
+from .simplewebsocket import SimpleWebSocket, JsonSerializingWebSocket
